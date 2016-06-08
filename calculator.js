9228e28e1c66344bf3ea636051d5edff6971fe86
Victor <viclee02@gmail.com>
2016-06-07 20:52:17 -1000

    refactor exercises

--- calculator.js
+++ calculator.js
@@ -6,128 +6,79 @@
  * @return {object} `calculator` object that can be used
  */
 
-  function calculatorModule(){
+function calculatorModule(){
+  var memory = 0;
+  var total = 0;
+  var calculator = {
+    load: load,
+    getTotal: getTotal,
+    add: add,
+    subtract: subtract,
+    multiply: multiply,
+    divide: divide,
+    recallMemory: recallMemory,
+    saveMemory: saveMemory,
+    clearMemory: clearMemory
+  };
 
-    //private variables can only be accessed by private functions and priviledged methods
-    var memory = 0;
-    var total = 0;
-
-    var calculator = {
-      load: _load,
-      getTotal : _getTotal,
-      add: _add,
-      subtract: _subtract,
-      multiply: _multiply,
-      divide: _divide,
-      recallMemory: _recallMemory,
-      saveMemory: _saveMemory,
-      clearMemory: _clearMemory,
-    };
-
-  /**
-   * sets the `total` to the number passed in
-   * @param  { Number } x
-   * @return { Number }    current total
-   */
-  
-    function _load(newTotal){
-      if(typeof (newTotal) === 'number'){ //typeof operator returns a string indicating the type of the unevaluated operand
-        total = newTotal;
-        return total;
-      } else {
-        return 'digits yo!';
-      }
-    }
-
-
-  /**
-   * Return the value of `total`
-   * @return { Number }
-   */
-   function _getTotal(){
-    return total;
-   }
-
-  /**
-   * Sums the value passed in with `total`
-   * @param { Number } x
-   */
-  
-  function _add(number){
-    if(typeof (number) === 'number'){
-      total += number;
+  function load(newTotal){
+    if(typeof(newTotal)=== 'number'){ //typeOf operator to find type of Javascript data type
+      total = newTotal;
+      return total;
     }else{
-      return 'digits yo!';
+      return "enter a number";
     }
   }
 
-  /**
-   * Subtracts the value passed in from `total`
-   * @param  { Number } x
-   */
+  function getTotal(){
+    return total;
+  }
 
-   function _subtract(number){
-    if(typeof (number) === 'number'){
+  function add(number){
+    if(typeof(number) === 'number'){
+      total += number;
+    }else{
+      return "enter a nunber";
+    }
+  }
+
+  function subtract(number){
+    if(typeof(number)=== 'number'){
       total -= number;
     }else{
-      return 'digits yo?';
+      return "enter a number";
     }
-   }
+  }
 
-  /**
-   * Multiplies the value by `total`
-   * @param  { Number } x
-   */
-
-   function _multiply(number){
-    if(typeof (number) === 'number'){
+  function multiply(number){
+    if(typeof(number)=== 'number'){
       total *= number;
     }else{
-      return 'digits yo!';
+      return 'enter a number';
     }
-   }
+  }
 
-  /**
-   * Divides the value passing in by `total`
-   * @param  { Number } x
-   */
-
-   function _divide(number){
-    if(typeof (number) === 'number'){
+  function divide(number){
+    if(typeof(number) === 'number'){
       total /= number;
     }else{
-      return 'digits yo!';
+      return 'enter a number';
     }
-   }
+  }
 
-  /**
-   * Return the value stored at `memory`
-   * @return { Number }
-   */
-
-   function _recallMemory(){
+  function recallMemory(){
     return memory;
-   }
+  }
 
-  /**
-   * Stores the value of `total` to `memory`
-   */
-  
-  function _saveMemory(){
+  function saveMemory(){
     memory = total;
   }
 
-
-  /**
-   * Clear the value stored at `memory`
-   */
-  
-  function _clearMemory(){
+  function clearMemory(){
     memory = 0;
   }
 
-  /**
-   * Validation
-   */
-   return calculator;
+
+  return calculator;
 }
+
